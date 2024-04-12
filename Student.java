83500b00999bbe5917222f71ec07c05e129fdaaf
Kacper <s31304@pjwstk.edu.pl>
2024-04-12 21:28:56 +0200

    all

--- Student.java
+++ Student.java
@@ -1,8 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Objects.isNull;
+
 public class Student {
     public String fname;
     public String lname;
-    public String indexNumber;
+    public Integer indexNumber;
     public String email;
-    public String adress;
-    public double[] grades;
+    public String address;
+    public List<Double> grades;
+
+    private static final int MIN_MEAN = 2;
+    private static final int MAX_MEAN = 5;
+    private static final int MAX_GRADES_SIZE = 20;
+
+    public Student() {
+        grades = new ArrayList<>();
+    }
+
+    public void addGrade(double grade) {
+        if (grades.size() == MAX_GRADES_SIZE) {
+            throw new IllegalStateException("Cannot add more grades");
+        }
+
+        grades.add(grade);
+    }
+
+    public double calculateMean() {
+        if (isNull(grades) || grades.isEmpty()) {
+            throw new IllegalArgumentException("Student has no grades.");
+        }
+
+        double sum = 0;
+        for (double grade : grades) {
+            sum = sum + grade;
+        }
+
+        double mean = sum / grades.size();
+
+        if (mean < MIN_MEAN) {
+            return MIN_MEAN;
+        } else if (mean > MAX_MEAN) {
+            return MAX_MEAN;
+        } else {
+            return (double) Math.round(mean * 2) / 2;
+        }
+    }
 }
--- StudentGroup.java
+++ StudentGroup.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentGroup {
+    public String name;
+    public List<Student> students;
+
+    private static final int MAX_GROUP_SIZE = 15;
+
+    StudentGroup() {
+        students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        if (students.contains(student)) {
+            throw new IllegalStateException("This student exists in group.");
+        }
+
+        if (students.size() == MAX_GROUP_SIZE) {
+            throw new IllegalStateException("Group is full.");
+        }
+
+        students.add(student);
+    }
 }
